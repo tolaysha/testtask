288c843429061a458ad7a3d9a4640e7ed1d8711b
Anatoly Kuzmin <tolaykuzay@gmail.com>
2020-02-26 21:14:09 +0300

    fetchUrl promise

--- src/store/kanban/saga.js
+++ src/store/kanban/saga.js
@@ -9,6 +9,32 @@ const delay = data => new Promise((resolve, reject) => {
   }, 1000);
 
 });
+const fetchUrl = url =>{
+  let count = 0;
+  console.log('fetchUrl');
+  return new Promise((resolve, reject)=>{
+      let reset = url =>{
+          fetch(url)
+              .then(res => {
+                  if (res.ok) {
+                      console.log('i am res.ok')
+                      return resolve(res)
+                  }else{
+                      console.log('i am bad')
+                      return reject(res)
+                  }})
+              .catch((reject)=>{
+                  console.log(reject)
+                  count<7?reset(url):Promise.reject();
+                  count++;
+                  console.log('misstake count',count)
+              })
+      }
+      console.log('i am before reset first')
+      reset(url)//.then(resolve).catch(reject);
+  })
+  
+}
 
 
 function* getItemsAsync() {
