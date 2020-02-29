6f3e8f6fa86748122d605fa1279cfac0e6a67f1f
Anatoly Kuzmin <tolaykuzay@gmail.com>
2020-02-29 05:02:44 +0300

    Updates

---
+++ precache-manifest.fd17c9f1c5652a1b567715057a8b79ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eed0329b9f4a89939b72d2570464be3",
+    "url": "/testtask/index.html"
+  },
+  {
+    "revision": "f6dd8448922ceb3301ee",
+    "url": "/testtask/static/css/main.eb0345e9.chunk.css"
+  },
+  {
+    "revision": "a5043f671500ef3d5c13",
+    "url": "/testtask/static/js/2.a450151e.chunk.js"
+  },
+  {
+    "revision": "e933f8dbfdf496cb72e495663a499a00",
+    "url": "/testtask/static/js/2.a450151e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6dd8448922ceb3301ee",
+    "url": "/testtask/static/js/main.c2a25639.chunk.js"
+  },
+  {
+    "revision": "cba6e3da9d5f714244ee",
+    "url": "/testtask/static/js/runtime-main.f87535d0.js"
+  }
+]);
