513865ffd1a6ae510bde28b16f8858ce353cb27f
Anatoly Kuzmin <tolaykuzay@gmail.com>
2020-02-29 18:12:02 +0300

    react-router-dom hooks

--- src/screens/kanban/kanban.jsx
+++ src/screens/kanban/kanban.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import ClassNames from 'classnames';
 import injectSheet from 'react-jss'
 import Paper from '../../components/paper/paper.jsx'
-import { useRouteMatch, } from "react-router-dom";
+import { useParams, } from "react-router-dom";
 import { useDispatch } from 'react-redux'
 import { getItems } from '../../store/kanban/actions.js'
 const styles = {
@@ -15,16 +15,16 @@ let Kanban = ({
   classes,
   ...other }) => {
 
-  let match = useRouteMatch();
+  let params = useParams();
   const dispatch = useDispatch();
-  console.log('i am math from hook useRouterMath from kanban.jsx', match);
+  console.log('i am params from hook useRouterMath from kanban.jsx', params);
   //const [Items, setItems] = useState();
   useEffect((state) => {
     console.log('i am state from useEffect-', state);
     console.log('i am dispatch in useEffect in kanban.jsx', dispatch);
     console.log('i am getItems in useEffect in kanban.jsx', getItems);
-    dispatch(getItems(Number(match.params.id)))
-  }, [match.params.id]);
+    dispatch(getItems(Number(params.id)))
+  }, [params.id]);
 
   console.log('console check __filename from Kanban.jsx', __filename);
   console.log('console check other from Kanban.jsx', other);
